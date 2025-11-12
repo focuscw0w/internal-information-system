bb8f8add0626663969068d6c50a507dacb4aa24c
filipj169 <jamrichfilip16@gmail.com>
2025-11-12 16:31:36 +0100

    adding route for main projects page

---
+++ Modules/Project/resources/js/pages/Projects/index.tsx
@@ -0,0 +1,7 @@
+import AppLayout from '../../../../../../resources/js/layouts/app-layout';
+
+export default function Index() {
+    return <AppLayout>
+        <h1>Project page</h1>
+    </AppLayout>
+}
--- Modules/Project/routes/web.php
+++ Modules/Project/routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use Modules\Project\Http\Controllers\ProjectController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('projects', ProjectController::class)->names('project');
 });
 
-Route::get('/project-test', function () {
-    return 'Project modul funguje!';
+// Main page for Projects module
+Route::middleware(['web'])->group(function () {
+    Route::get('/projects', fn () => Inertia::render('Projects/Index'))
+        ->name('projects.index');
 });
